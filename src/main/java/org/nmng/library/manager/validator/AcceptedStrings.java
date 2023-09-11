3fcd0907f04c6152e487aca263f162a459cd00bd
nmng108 <namng.108@gmail.com>
2023-09-12 05:25:57 +0700

    Update 5.1: add sequence matching validator

---
+++ src/main/java/org/nmng/library/manager/validator/AcceptedStrings.java
@@ -0,0 +1,20 @@
+package org.nmng.library.manager.validator;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+import jakarta.validation.ReportAsSingleViolation;
+
+import java.lang.annotation.*;
+
+@Documented
+@Constraint(validatedBy = AcceptedStringsValidator.class)
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+//@NotNull(message = "Value cannot be null")
+@ReportAsSingleViolation
+public @interface AcceptedStrings {
+    String[] value();
+    String message() default "Value is not allowed";
+    Class<?>[] groups() default { };
+    Class<? extends Payload>[] payload() default { };
+}
